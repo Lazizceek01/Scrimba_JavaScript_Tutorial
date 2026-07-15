8ac4eb8ecbc9be85f8d641b63a311ee385a6462f
Lazizceek01 <azizovlaziz33@gmail.com>
2026-07-15 11:29:09 +0500

    Reassigning and incrementing

---
+++ Lesson-4/index.js
@@ -0,0 +1,40 @@
+// document.getElementById("count").innerText = 5
+
+let count = 5
+count = count + 1
+console.log(count)
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+
+let bonusPoints = 50 
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints) 
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 45 
+console.log(bonusPoints)
+
+
+
+
+// My opinion but it is uncorrect 
+
+// // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// // Decrease it down to 25, and then finally increase it to 70
+// // Console.log the value after each step
+
+// // First step
+// let bonusPoints = 50;
+// console.log(bonusPoints);
+// // Second step
+// bonusPoints = 100;
+// console.log(bonusPoints);
+// // Next step 
+// bonusPoints = 25;
+// console.log(bonusPoints);
+// // finally step 
+// bonusPoints = 70;
+// console.log(bonusPoints);
